2bbc16f301895174a25014fc9d7f8359b1226a0d
James Billingham <james@billingham.net>
2014-09-10 09:29:15 +0100

    Added test infrastructure to support a mocked API

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var test = require('tape');
+var http = require('http');
 var request = require('supertest');
 var app = require('../app');
 
@@ -58,3 +59,43 @@ test('message creation fails correctly', function (t) {
 		.expect('Location', '/messages/new?failure')
 		.end(t.error);
 });
+
+// will generate 3 assertions for each request it handles
+function setupTestServer(t, handler, callback) {
+	var token = 'arbitrary-non-existing-token';
+	var originalUrl = app.get('service_url');
+	var originalToken = app.get('service_token');
+
+	var server = http.createServer(function (request, response) {
+		t.equal(request.method, 'POST');
+		t.equal(request.url, '/messages');
+		t.equal(request.headers['authorization'], 'Bearer ' + token);
+
+		body = '';
+
+		request.on('data', function (chunk) {
+			body += chunk;
+		});
+
+		request.on('end', function () {
+			handler(request, response, body);
+		});
+	});
+
+	server.listen(function () {
+		var addr = server.address();
+		var url = 'http://' + addr.address + ':' + addr.port;
+
+		app.set('service_url', url);
+		app.set('service_token', token);
+
+		callback();
+	});
+
+	server.on('close', function () {
+		app.set('service_url', originalUrl);
+		app.set('service_token', originalToken);
+	});
+
+	return server;
+}
